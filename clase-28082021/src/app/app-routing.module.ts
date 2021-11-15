2929098c5cbc83028b4aa39d71fe3d0a4d657eee
abrahamalvina <86324493+abrahamalvina@users.noreply.github.com>
2021-11-14 23:27:26 -0300

    creación de pages

--- clase-28082021/src/app/app-routing.module.ts
+++ clase-28082021/src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'detalle',
+    loadChildren: () => import('./pages/detalle/detalle.module').then( m => m.DetallePageModule)
+  },
 ];
 
 @NgModule({
