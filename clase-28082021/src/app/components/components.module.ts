5b3458c0457ecf3f272e267db34e3800cbe02f37
abrahamalvina <86324493+abrahamalvina@users.noreply.github.com>
2021-11-15 21:31:08 -0300

    Creación de: Páginas, modulos.

--- clase-28082021/src/app/components/components.module.ts
+++ clase-28082021/src/app/components/components.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { IonicModule } from '@ionic/angular'
+import { EncabezadoComponent } from './encabezado/encabezado.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    EncabezadoComponent
+
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule
+  ],
+
+  exports:[
+    EncabezadoComponent
   ]
 })
 export class ComponentsModule { }
